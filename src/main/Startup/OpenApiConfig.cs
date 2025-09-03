95456f3d77613d1b5ba5db6c227f6ab0407b970c
jinwoo2-inkcode <jasonkim0808@gmail.com>
2025-09-03 11:34:59 -0500

    added gitignore file

--- src/main/Startup/OpenApiConfig.cs
+++ src/main/Startup/OpenApiConfig.cs
@@ -7,10 +7,6 @@ using System.Text;
 
 public static class OpenAPIConfig
 {
-
-    //connection string variable - did not decide where to put this
-    public static string ConnectionString = "Data Source=FREEPDB1;Initial Catalog=MES;User id=dbo;Password=Duksu123!;";
-
     public static void AddOpenApiServices(this IServiceCollection services)
     {
         services.AddOpenApi();
